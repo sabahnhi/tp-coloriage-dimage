226659e49077144898450fe7e4bcf61b45ab54af
Sabahnhi <sabah.nahoui@bellevue-lyon.org>
2020-04-22 11:57:39 +0200

    Code à exécuter

---
+++ dossier_code/coloriage_image.py
@@ -0,0 +1,9 @@
+# On a besoin de récupérer la longueur (entier) et la largeur (entier) de l'utilisateur :
+
+print("Saisir la longueur (entier)")
+longueur_saisie = input()
+print(type(longueur_saisie))
+longueur = int(longueur_saisie)
+print("Saisir la largeur (entier)")
+largeur = float(input())
+
